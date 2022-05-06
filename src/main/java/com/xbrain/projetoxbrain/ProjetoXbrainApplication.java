fca6f064ca8afb37ef9f37f934d447af904c228c
Rafael Sulimann <sulimannrafael@gmail.com>
2022-05-06 15:51:02 -0300

    Criação do projeto + dependências no arquivo pom.xml

---
+++ src/main/java/com/xbrain/projetoxbrain/ProjetoXbrainApplication.java
@@ -0,0 +1,13 @@
+package com.xbrain.projetoxbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoXbrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoXbrainApplication.class, args);
+	}
+
+}
